c106b5f28caa6116d23b26907288646aaa9377a3
Gauri764 <gauripramanik7@gmail.com>
2025-07-17 13:27:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title('License Management Software')
 
 st.write('Hello world!')
+
+import pandas as pd
+
+df = pd.read_csv('synthetic_licensing_dataset.csv')
+st.dataframe(df)
+
